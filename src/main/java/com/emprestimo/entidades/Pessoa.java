5fa39b207455a0d72ac737319c638a729781d2c9
Edson Sales <edsonsales99@gmail.com>
2021-12-24 10:27:06 -0500

    Criando as entidades pessoa e endereço

---
+++ src/main/java/com/emprestimo/entidades/Pessoa.java
@@ -0,0 +1,15 @@
+package com.emprestimo.entidades;
+
+import java.util.List;
+
+public class Pessoa {
+    private long idPessoa;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String rg;
+    private Double renda;
+    private String senha;
+    private List<Endereco> endereco;
+
+}
