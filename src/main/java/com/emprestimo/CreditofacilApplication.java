cdbad77a09f37622f00bddd5fd8bbf3cc328e698
Edson Sales <edsonsales99@gmail.com>
2021-12-24 10:10:08 -0500

    Criando a estrutura  inicial do projeto

---
+++ src/main/java/com/emprestimo/CreditofacilApplication.java
@@ -0,0 +1,13 @@
+package com.emprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditofacilApplication.class, args);
+	}
+
+}
