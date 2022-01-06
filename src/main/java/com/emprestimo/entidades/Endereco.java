369a7cead9c33e566292ec69c6fd38816ee6f9e6
fernando Iwerson <fernandoiwerson@gmail.com>
2022-01-06 19:32:29 -0300

    first commit

--- src/main/java/com/emprestimo/entidades/Endereco.java
+++ src/main/java/com/emprestimo/entidades/Endereco.java
@@ -1,46 +1,30 @@
 package com.emprestimo.entidades;
 
-import javax.persistence.*;
 import java.util.Objects;
 
-@Entity
-@Table(name  = "tb_endereco")
 public class Endereco {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idEndereco;
-
     private String uf;
     private String cidade;
     private String bairro;
     private String rua;
     private int numero;
+    private Pessoa pessoa;
 
-    @ManyToOne
-    @JoinColumn(name = "idPessoa")
-    private Pessoa pessoaEndereco;
 
-    /**
-     * Construtor sem argumentos*/
     public Endereco() {
+
     }
 
-    /**
-     * Construtor com argumentos*/
-    public Endereco(Long idEndereco, String uf, String cidade, String bairro, String rua, int numero, Pessoa pessoaEndereco) {
+    public Endereco(Long idEndereco, String uf, String cidade, String bairro, String rua, int numero, Pessoa pessoa) {
         this.idEndereco = idEndereco;
         this.uf = uf;
         this.cidade = cidade;
         this.bairro = bairro;
         this.rua = rua;
         this.numero = numero;
-        this.pessoaEndereco = pessoaEndereco;
+        this.pessoa = pessoa;
     }
-
-    /**
-     * Construtor com argumentos*/
-
 
     public Long getIdEndereco() {
         return idEndereco;
@@ -90,12 +74,12 @@ public class Endereco {
         this.numero = numero;
     }
 
-    public Pessoa getPessoaEndereco() {
-        return pessoaEndereco;
+    public Pessoa getPessoa() {
+        return pessoa;
     }
 
-    public void setPessoaEndereco(Pessoa pessoaEndereco) {
-        this.pessoaEndereco = pessoaEndereco;
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
     }
 
     @Override
@@ -103,15 +87,12 @@ public class Endereco {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Endereco endereco = (Endereco) o;
-        return numero == endereco.numero && Objects.equals(idEndereco, endereco.idEndereco)
-                && Objects.equals(uf, endereco.uf) && Objects.equals(cidade, endereco.cidade)
-                && Objects.equals(bairro, endereco.bairro) && Objects.equals(rua, endereco.rua)
-                && Objects.equals(pessoaEndereco, endereco.pessoaEndereco);
+        return numero == endereco.numero && Objects.equals(idEndereco, endereco.idEndereco) && Objects.equals(uf, endereco.uf) && Objects.equals(cidade, endereco.cidade) && Objects.equals(bairro, endereco.bairro) && Objects.equals(rua, endereco.rua) && Objects.equals(pessoa, endereco.pessoa);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(idEndereco, uf, cidade, bairro, rua, numero, pessoaEndereco);
+        return Objects.hash(idEndereco, uf, cidade, bairro, rua, numero, pessoa);
     }
 
     @Override
@@ -123,7 +104,7 @@ public class Endereco {
                 ", bairro='" + bairro + '\'' +
                 ", rua='" + rua + '\'' +
                 ", numero=" + numero +
-                ", pessoa=" + pessoaEndereco +
+                ", pessoa=" + pessoa +
                 '}';
     }
 }
--- src/main/java/com/emprestimo/entidades/Pessoa.java
+++ src/main/java/com/emprestimo/entidades/Pessoa.java
@@ -1,16 +1,8 @@
 package com.emprestimo.entidades;
 
-import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-@Entity
-@Table(name = "tb_pessoa")
 public class Pessoa {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idPessoa;
     private String nome;
     private String email;
@@ -18,135 +10,6 @@ public class Pessoa {
     private String rg;
     private Double renda;
     private String senha;
+    private List<Endereco> endereco;
 
-    @OneToMany(mappedBy = "pessoaEmprestimo")
-    private List<Emprestimo> emprestimos = new ArrayList<>();
-
-    @OneToMany(mappedBy =  "pessoaEndereco")
-    private List<Endereco> enderecos  = new ArrayList<>();
-
-    /**
-     * Construtor sem argumentos*/
-    public Pessoa() {
-    }
-
-    /**
-     * Construtor com argumentos*/
-    public Pessoa(long idPessoa, String nome, String email, String cpf, String rg, Double renda, String senha, List<Emprestimo> emprestimos, List<Endereco> enderecos) {
-        this.idPessoa = idPessoa;
-        this.nome = nome;
-        this.email = email;
-        this.cpf = cpf;
-        this.rg = rg;
-        this.renda = renda;
-        this.senha = senha;
-        this.emprestimos = emprestimos;
-        this.enderecos = enderecos;
-    }
-
-    /**
-     * Getters and setters*/
-    public long getIdPessoa() {
-        return idPessoa;
-    }
-
-    public void setIdPessoa(long idPessoa) {
-        this.idPessoa = idPessoa;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public Double getRenda() {
-        return renda;
-    }
-
-    public void setRenda(Double renda) {
-        this.renda = renda;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public List<Emprestimo> getEmprestimos() {
-        return emprestimos;
-    }
-
-    public void setEmprestimos(List<Emprestimo> emprestimos) {
-        this.emprestimos = emprestimos;
-    }
-
-    public List<Endereco> getEnderecos() {
-        return enderecos;
-    }
-
-    public void setEnderecos(List<Endereco> enderecos) {
-        this.enderecos = enderecos;
-    }
-
-    /**
-     * equals da classe*/
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pessoa pessoa = (Pessoa) o;
-        return idPessoa == pessoa.idPessoa && Objects.equals(nome, pessoa.nome) && Objects.equals(email, pessoa.email) && Objects.equals(cpf, pessoa.cpf) && Objects.equals(rg, pessoa.rg) && Objects.equals(renda, pessoa.renda) && Objects.equals(senha, pessoa.senha) && Objects.equals(emprestimos, pessoa.emprestimos) && Objects.equals(enderecos, pessoa.enderecos);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(idPessoa, nome, email, cpf, rg, renda, senha, emprestimos, enderecos);
-    }
-
-    /**
-     *toString da classe */
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                "idPessoa=" + idPessoa +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", rg='" + rg + '\'' +
-                ", renda=" + renda +
-                ", senha='" + senha + '\'' +
-                ", emprestimos=" + emprestimos +
-                ", enderecos=" + enderecos +
-                '}';
-    }
 }
